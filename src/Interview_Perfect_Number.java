2d7501a0f5c654507025df1ec4b281783f14e124
osgingbena <oshingbenga@gmail.com>
2018-02-18 13:35:25 +0000

    Added project update

--- src/Interview_Perfect_Number.java
+++ src/Interview_Perfect_Number.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Interview_Perfect_Number {
 
+
     public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int a =kb.nextInt();
+        int b = kb.nextInt();
         int n = 1;
+        //int a = 33550300;
+        //int b = 33550400;
 
         System.out.println("perfect number between 33550300 and 33550400");
-        for(int i = 33550300; i<=33550400; i++) {
+        for(int i = a; i<=b; i++) {
             int sum = 0;
             for (n = 1; n < i; n++) {
                 if (i % n == 0) {
