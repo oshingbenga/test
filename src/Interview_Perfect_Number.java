92b657935d8fe74d2cce540e67d0ec97b5a65af9
osgingbena <oshingbenga@gmail.com>
2018-02-19 09:01:44 +0000

    Added project update2

--- src/Interview_Perfect_Number.java
+++ src/Interview_Perfect_Number.java
@@ -9,10 +9,8 @@ public class Interview_Perfect_Number {
         int a =kb.nextInt();
         int b = kb.nextInt();
         int n = 1;
-        //int a = 33550300;
-        //int b = 33550400;
 
-        System.out.println("perfect number between 33550300 and 33550400");
+        System.out.println("perfect number between two numbers");
         for(int i = a; i<=b; i++) {
             int sum = 0;
             for (n = 1; n < i; n++) {
